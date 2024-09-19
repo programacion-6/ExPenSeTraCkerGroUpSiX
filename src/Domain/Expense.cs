af58dfd608baac3995ed7103ed520cfbe19411e3
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2024-09-19 17:08:52 -0400

    feat: implement Expense class

---
+++ src/Domain/Expense.cs
@@ -0,0 +1,10 @@
+namespace ExpenseTrackerGrup6.src.Domain;
+
+public class Expense
+{
+    public required Guid Id { get; set; }
+    public required decimal Amount { get; set; }
+    public required DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required ExpenseCategory Category { get; set; }
+}
