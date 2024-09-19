ded1580dc721407454af5528732a1ec7df6bb5d1
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2024-09-19 17:09:20 -0400

    feat: implement Income class

---
+++ src/Domain/Income.cs
@@ -0,0 +1,9 @@
+namespace ExpenseTrackerGrup6.src.Domain;
+
+public class Income
+{
+    public required Guid Id { get; set; }
+    public required decimal Amount { get; set; }
+    public required DateTime Date { get; set; }
+    public required string Source { get; set; }
+}
